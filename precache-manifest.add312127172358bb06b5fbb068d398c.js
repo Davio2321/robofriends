75f96e3e7a0ca10e2dee50a09c63501e6b2a4b05
David Middleton <middleton.davida@gmail.com>
2019-11-07 22:30:18 -0500

    Updates

---
+++ precache-manifest.add312127172358bb06b5fbb068d398c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b44b8cf98ae07178a2ebe1c74c4403",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "98e2b7285fcf401b484e",
+    "url": "/robofriends/static/css/main.0bb17b57.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "98e2b7285fcf401b484e",
+    "url": "/robofriends/static/js/main.70d93f33.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
